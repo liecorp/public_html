905953f8943a904d58c937ba1aafbef4f1163642
Hendrik Lie <hendriklie72@gmail.com>
2023-09-02 20:23:27 +0700

    refactor css, add more example contents, and restructure html
    Refactor css:
    - reduce duplication
    - rearrange classes hierarchy
    - reduce reliance to id, instead focus on classes
    - reduces the use of px in favor of em
    
    add contents to contents.js:
    - contents are now in arrays for easy debugging and testing
    - add more types of test contents for display testing
    
    restructure html:
    - arrange html skeleton to improve the appearances
    - aiming for high modularity of contents

--- share/contents.js
+++ share/contents.js
@@ -1,6 +1,6 @@
 let contentBlockPrimary = document.getElementById("contentBlockPrimary");
 let contentBlockSecondary = document.getElementById("contentBlockSecondary");
-
+let nginxWelcome = document.getElementById('nginxWelcome');
 let navItemBtn = new PageNavItem('<i class="fa fa-bars"></i>','javascript:void(0)','nav-icon','navBarToggle()');
 
 let navItems = [
@@ -10,9 +10,8 @@ let navItems = [
     new PageNavItem('Contact','#Contact'),
 ]
 
-let blockPrimary = new PageItem(
-    'div',
-    `<h1>Welcome to Nginx!</h1>
+let pageBlocks = [
+    `<h2>Welcome to Nginx!</h2>
     <p>
     If you see this page, the nginx web server is successfully installed and working.
         Further configuration is required.
@@ -23,24 +22,52 @@ let blockPrimary = new PageItem(
         <a class="navbar-link" class="link-light" href="http://nginx.com/">nginx.com</a>.</p>
 
     <p><em>Thank you for using nginx.</em></p>`,
-    'block-primary'
-    )
-
-let blockSecondary = new PageItem(
-    'div',
-    `<h1>
+    `<h2>
         Secondary Block
-    </h1>
+    </h2>
     <p>
         The content should not matter much.
         What is important is that this section should not be a primary one.
         It should contain extra contents not vital to be not present.
-        </p>`,
+    </p>`,
+    `<h2 class="heading">A simple, lightweight webpage</h2>
+
+    <p>You've reached the website for <strong>liecorp.id</strong>, hosted in a
+    lightweight and flexible Linux server that tries to Keep It
+    Simple.</p>
+
+    <p>Currently we have several pages live and available to access from our simple
+    navigation bar. Feel free to check for them. Though note that they are mostly
+    work in progress.</p>
+
+    <p>This page is served with an nginx web server. Some of the other sites here are
+    served with nginx inside docker containers, especially the ones with php as their
+    backend. Those sites served from within docker containers are then configured to
+    their respective subdomains with native nginx web server and simple reverse proxy
+    configurations.</p>`
+]
+
+let blockPrimary = new PageItem(
+    'div',
+    pageBlocks[2],
+    'block-primary'
+)
+
+let blockSecondary = new PageItem(
+    'div',
+    pageBlocks[1],
     'block-secondary'
-    )
+)
+
+let blockNginxWelcome = new PageItem(
+    'div',
+    pageBlocks[0],
+    'block-nginx'
+)
 
 generateContent(contentBlockPrimary,blockPrimary)
 generateContent(contentBlockSecondary,blockSecondary)
+generateContent(nginxWelcome,blockNginxWelcome)
 
 // If navItemBtn is defined, use:
 generateNavContents(navItems,navItemBtn);
