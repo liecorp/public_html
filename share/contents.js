d24eab2c237c18b07901161ccb8a66187578626d
Hendrik Lie <hendriklie72@gmail.com>
2023-09-02 16:43:03 +0700

    WIP: make navbar aware of url path, adjust active state accordingly

--- share/contents.js
+++ share/contents.js
@@ -1,12 +1,38 @@
 let navItemBtn = new PageNavItem('<i class="fa fa-bars"></i>','javascript:void(0)','nav-icon','navBarToggle()');
 
 let navItems = [
-    new PageNavItem('Home','#Home'),
-    new PageNavItem('Blog','#Blog'),
-    new PageNavItem('About','#About'),
-    new PageNavItem('Contact','#Contact'),
+    new PageNavItem('Home','vanilla.html'),
+    new PageNavItem('Blog','vanilla.html#Blog'),
+    new PageNavItem('About','vanilla.html#About'),
+    new PageNavItem('Contact','vanilla.html#Contact'),
 ]
 // If navItemBtn is defined, use:
 generateNavContents(navItems,navItemBtn);
 // Otherwise, use:
 // generateNavContents(navItems);
+
+function updateNavContent(origNav,originHref) {
+    // fullGetString = [this.location.pathname,this.location.hash].join('');
+    console.log(origNav);
+    let updatedNav = origNav.map((item) => {
+        if (`${window.location.origin}/${item.itemURL}` === originHref) {
+            if (typeof(item.itemClass) === 'string') {
+                item.itemClass += ' active';
+            } else { item.itemClass = 'active'; }
+            return item;
+        }
+        return item;
+    })
+    generateNavContents(updatedNav,navItemBtn)
+    console.log(origNav === updatedNav)
+}
+
+document.addEventListener(`click`, e => {
+    const origin = e.target.closest(`a`);
+    
+    if (origin) {
+        // console.clear();
+        console.log(`You clicked ${origin.href}`);
+        updateNavContent(navItems,origin.href);
+    }
+});
