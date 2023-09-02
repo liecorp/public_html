0c284ddf9cb204a2bfd1fae9be5f3a8b77574a48
Hendrik Lie <hendriklie72@gmail.com>
2023-09-03 04:58:17 +0700

    example contents for news collection arrays

--- share/index.js
+++ share/index.js
@@ -57,12 +57,18 @@ let contentObjects = [
 let newsClasses = makeNewsItemClass('content-news-head','content-timestamp','content-news-body');
 let newsTitles = [
     'ansible-core &gt;= 2.15.3-1 update may require manual intervention',
+    'budgie-desktop &gt;= 10.7.2-6 update requires manual intervention',
+    'TeX Live package reorganization',
 ];
 let newsUrls = [
     "/news/ansible-core-2153-1-update-may-require-manual-intervention/",
+    "/news/budgie-desktop-1072-6-update-requires-manual-intervention/",
+    "/news/tex-live-package-reorganization/",
 ]
 let newsStamps = [
     new Date('2023-08-19 03:25:22 UTC+8'),
+    new Date('2023-08-11 03:53:45 UTC+7'),
+    new Date('2023-06-18 1:36:27 UTC+3'),
 ];
 let newsBodies  = [
     `<p>As of <code>ansible-core 2.15.3</code>, upstream moved documentation and examples to a separate <a href="https://github.com/ansible/ansible-documentation">dedicated repository</a> (see the <a href="https://github.com/ansible/ansible/blob/v2.15.3/changelogs/CHANGELOG-v2.15.rst#minor-changes">related changelogs</a>).<br />
@@ -73,11 +79,30 @@ let newsBodies  = [
     <p>After updating from <code>ansible-core</code> &lt;= <code>2.15.2-1</code> to &gt;= <code>2.15.3-1</code>, everyone using a <strong>custom</strong> global Ansible configuration file stored under <code>/etc/ansible/ansible.cfg</code> will have their configuration saved as a <code>pacsave</code> file.<br />
     To restore it, run the following command:</p>
     <p><code>mv /etc/ansible/ansible.cfg.pacsave /etc/ansible/ansible.cfg</code></p>`,
+    `<p>When upgrading from budgie-desktop 10.7.2-5 to 10.7.2-6, the package mutter43 must be replaced with magpie-wm, which currently depends on mutter. As mutter43 conflicts with mutter, manual intervention is required to complete the upgrade.</p>
+    <p>First remove mutter43, then immediately perform the upgrade. Do not relog or
+    reboot between these steps.</p>
+    <p><code>pacman -Rdd mutter43</code></p>
+    <p><code>pacman -Syu</code></p>`,
+    `<p>Starting from version 2023.66594-9, TeX Live packages have been reorganized to mirror upstream collections. Even though the new <code>texlive-basic</code> replaces the old <code>texlive-core</code>, many of the texlive-core contents (including language specific files) are now split between different packages. To find out which Arch package contains a specific CTAN package, you can use the <code>tlmgr</code> utility, eg.</p>
+<pre><code>$ tlmgr info euler | grep collection
+collection:  collection-latexrecommended</code></pre>
+    <p>which means the euler CTAN package is contained in <code>texlive-latexrecommended</code>. You may also use <code>pacman -F</code> to query for specific files.</p>
+    <p>A new metapackage texlive-meta is available to install all subpackages (except for …</p>`,
 ]
-newsItems = document.getElementById('newsItems');
-newsContent = new NewsItem(newsTitles[0],newsStamps[0],newsBodies[0],newsUrls[0]);
+let newsItems = document.getElementById('newsItems');
+let newsContent = new NewsItem(newsTitles[0],newsStamps[0],newsBodies[0],newsUrls[0]);
 
-generateNewsItem(newsItems,newsContent);
+let newsCollections = [];
+
+for ( let i = 0 ; i < newsTitles.length ; i++ ) {
+    newsCollections.push(
+        new NewsItem(newsTitles[i],newsStamps[i],newsBodies[i],newsUrls[i])
+    );
+}
+
+// generateNewsItem(newsItems,newsContent);
+generateNewsItem(newsItems,newsCollections);
 
 generateContent(nginxWelcome,contentObjects[0]);
 generateContent(infoPanel,contentObjects[1]);
