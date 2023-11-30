3f2999aedae3cdf6ba4d66adb50c0440cbb5b2bc
heno72 <hendriklie72@gmail.com>
2023-11-30 13:05:52 +0700

    refactor functions inside nav.js
    - Navbar toggle now evaluates classes as arrays instead of strings.
    - Removes unused codes and comments
    - Refactor makeNav() function, tidying up code.

--- share/nav.js
+++ share/nav.js
@@ -1,55 +1,49 @@
+// Toggles responsiveness of a given element ID
+// Takes two arguments:
+// - targetID = element ID of target
+// - responsiveClass = class name for responsiveness, defaults to 'responsive'
+// if responsiveClass is absent, add to element classes.
+// Otherwise removes responsiveClass
+function toggleResponsive(targetID, responsiveClass = 'responsive') {
+    let target = document.getElementById(targetID);
+    let targetArr = target.className.split(' ');
+    let isResponsive = function (txt) { return txt === responsiveClass } ;
+
+    if (!targetArr.find(isResponsive)) {
+        targetArr.push(responsiveClass);
+    } else {
+        targetArr = targetArr.filter(c => c !== responsiveClass);
+    }
+    target.className = targetArr.join(' ');
+}
+
 // A single source of truth for navbar
 /*  Toggle between adding and removing the "responsive" class
 to top-navbar when the user clicks on the icon */
 function navBarToggle() {
-    let navCont = document.getElementById("navContainer");
-    let topNavbar = document.getElementById("topNavbar");
-    if (topNavbar.className === "top-navbar") {
-        topNavbar.className += " responsive";
-    } else {
-        topNavbar.className = "top-navbar";
-    }
-    if (navCont.className === "nav-container") {
-        navCont.className += " responsive";
-    } else {
-        navCont.className = "nav-container";
-    }
+    toggleResponsive('topNavbar');
+    toggleResponsive('navContainer');
 }
 
 // Contact api endpoint and make navbar
 // optionally takes a custom nav button as its second argument
 async function makeNav(navApi, navBtn = 'default') {
     fetch(navApi)
+        // if endpoint is resolved, convert to json
+        .then(item => { if (item.ok) return item.json() })
+        // map item into objects of class PageNavItem
         .then(item => {
-            // if endpoint is resolved, convert to json
-            if (item.ok) {
-                return item.json()
-            }
-        })
-        .then(item => {
-            // map item into objects of class PageNavItem
             return item.map(obj => {
                 return new PageNavItem(obj.name,obj.route,obj.navclass)
             });
         })
+        // if mapping is successful, generate contents of navbar
         .then(nav => {
-            // if mapping is successful, generate contents of navbar
             return generateContentNav(nav, navBtn);
         })
         // insert into navbar
         .then(navbar => injectHTML('topNavbar',navbar))
         .catch((e) => { return 'error:' + e.message; });
 }
-
-let navItemBtn = new PageNavItem('<i class="fa fa-bars"></i>','javascript:void(0)','nav-icon','navBarToggle()');
-
-let navItems = [
-    new PageNavItem('Home','/','nav-home'),
-    new PageNavItem('Blog','/pages/blog.html','nav-blog'),
-    new PageNavItem('Wiki','/pages/wiki.html','nav-wiki'),
-    new PageNavItem('About','/pages/about.html','nav-about'),
-    // new PageNavItem('Xenowiki','https://xenomancy.id'),
-    // new PageNavItem('Sharebook','https://sharebook.liecorp.id/book/'),
-]
 
 makeNav('https://liecorp.id/api/navbar')
